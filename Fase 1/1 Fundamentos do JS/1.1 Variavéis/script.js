4578dba764d5753891c1a1c752ef86bbfe576082
Cris2021UT <112227932+crisfrota-menezes@users.noreply.github.com>
2025-04-18 22:52:29 -0300

    Re organizando as pastas
    Deixando apenas os projetos, do curso que são os exercícios de aplicação do que fui aprendendo.

--- Fase 1/1 Fundamentos do JS/1.1 Variavéis/script.js
+++
@@ -1,8 +0,0 @@
-let button = document.querySelector("button");
-
-button.onclick = function () {
-  let nome = prompt("Qual é o seu nome?");
-  alert("Olá " + nome + ", seja bem-vindo ao nosso site!");
-};
-
-// Exercicios feitos no mdn https://developer.mozilla.org/pt-BR/docs/Learn_web_development/Core/Scripting/Test_your_skills:_variables
